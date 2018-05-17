69fa4c88e522af4eeb4afba1cbf8d61ed9d19134
Hellscream360 <radioactiveteam360@gmail.com>
2018-05-17 14:14:21 +0200

    Finalize forgot password component

--- src/components/PasswordForget.js
+++ src/components/PasswordForget.js
@@ -1,8 +1,78 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
-const PasswordForget = () =>
+import { auth } from "../firebase";
+
+const PasswordForgetPage = () =>
   <div className="container">
     <h1>Mot de passe oublié</h1>
+    <PasswordForgetForm />
   </div>
 
-export default PasswordForget;
+const byPropKey = (propertyName, value) => () => ({
+  [propertyName]: value
+});
+
+const INITIAL_STATE = {
+  email: "",
+  error: null
+};
+
+class PasswordForgetForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...INITIAL_STATE };
+  }
+
+  onSubmit = (event) => {
+    const { email } = this.state;
+
+    auth.doPasswordReset(email)
+      .then(() => {
+        this.setState(() => ({ ...INITIAL_STATE }));
+      })
+      .catch(error => {
+        this.setState(byPropKey("error", error));
+      });
+    
+    event.preventDefault();
+  }
+  
+  render() {
+    const {
+      email,
+      error
+    } = this.state;
+
+    const isInvalid = email === "";
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          value={this.state.email}
+          onChange={event => this.setState(byPropKey("email", event.target.value))}
+          type="text"
+          placeholder="Email"
+        />
+        <button disabled={isInvalid} type="submit">
+          Changer mon mot de passe
+        </button>
+
+        { error && <p>{error.message}</p> }
+      </form>
+    )
+  }
+}
+
+const PasswordForgetLink = () => 
+  <p>
+    <Link to="/pw-forget">Mot de passe oublié ?</Link>
+  </p>
+
+export default PasswordForgetPage;
+
+export {
+  PasswordForgetForm,
+  PasswordForgetLink
+}
