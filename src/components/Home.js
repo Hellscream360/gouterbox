6df9f26f66b07079988789fd63bd5185b612f3dd
Hellscream360 <radioactiveteam360@gmail.com>
2018-05-18 13:37:12 +0200

    Delete name in browse.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,39 +1,12 @@
 import React from 'react';
 import withAuthorization from "./withAuthorization";
 import Browse from './browse';
-import userContext from "./AuthUserContext";
-import { db } from '../firebase';
 
 class HomePage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      users: null,
-    };
-  }
-
-  componentDidMount() {
-    db.onceGetUsers().then(snapshot =>
-      this.setState(() => ({ users: snapshot.val() }))
-    );
-  }
-
   render()  {
-    const { users } = this.state;
-    let name;
-
-    for ( let user in users )
-    {
-      if (users.hasOwnProperty(user))
-      {
-        name = users[user].username;
-      }
-    }
-    
     return (
       <div className="container">
-        <Browse name={name}/>
+        <Browse />
       </div>
     );
   }
