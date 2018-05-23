47a2d05044908330871e52ee4cd8d60975cc0a6f
adesousa <andre.desousa.95@gmail.com>
2018-05-23 22:34:49 +0200

    A change in the node version for the deploy in travis 6 now 7

--- src/components/landing/Presentation.js
+++ src/components/landing/Presentation.js
@@ -4,7 +4,7 @@ const Presentation = () =>
   <section className="presentation">
     <div className="masked-content">
       <span id="section1" className="anchor"></span>
-      <h1>Ô Goûter</h1>
+      <h1>Ô Goûter, mais avant un café pour André !</h1>
       <h4>Sain, équilibré, local, bio et bon !</h4>
       <h4>Livrés chaque jour à l'école</h4>
     </div>
