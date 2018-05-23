47dbeb8e26ecfff4c71926d98a12c0483b1336b8
adesousa <andre.desousa.95@gmail.com>
2018-05-23 22:26:09 +0200

    Pay a coffee to André, because now we are able to build by each git push thanks to travis-ci

---
+++ scripts/deploy.js
@@ -0,0 +1,11 @@
+const deployer = require("travis-ftpr");
+ 
+deployer.deploy({
+    host: process.argv[2],
+    username: process.argv[3],
+    password: process.argv[4],
+    localDirectory: process.argv[5],
+    remoteDirectory: process.argv[6]
+}, function() {
+    console.log("Go to Starbucks and pay a coffee to André because he nailed it !!");
+});
